861403cc0fe201a523414b8eef11a40b435d51bb
bluemonki <bluemonki@gmail.com>
2026-02-07 19:35:35 +0000

    Add in H2 DB instead of a hashmap and give tests their own properties to use in memory db

---
+++ src/main/java/com/tximpact/urlshortenr/repository/UrlRepository.java
@@ -0,0 +1,17 @@
+package com.tximpact.urlshortenr.repository;
+
+import com.tximpact.urlshortenr.entity.UrlToShorten;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// Annotation
+@Repository
+
+// Class
+public interface UrlRepository
+        extends JpaRepository<UrlToShorten, Long> {
+    List<UrlToShorten> findByFullUrl(String fullUrl);
+    List<UrlToShorten> findByShortUrl(String shortUrl);
+}
